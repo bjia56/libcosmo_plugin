f2f702b59edcfa61609de542be7b4e2a7a916f26
Brett Jia <dev.bjia56@gmail.com>
2025-01-08 16:39:33 -0500

    update linkage

--- include/cosmo_plugin.hpp
+++ include/cosmo_plugin.hpp
@@ -15,7 +15,24 @@ typedef SSIZE_T ssize_t;
 #endif
 
 #ifndef __COSMOPOLITAN__
-extern "C" void cosmo_rpc_initialization(int toPlugin, int toHost);
+
+#if defined(_MSC_VER)
+    //  Microsoft
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
+#else
+    //  do nothing and hope for the best?
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
+extern "C" EXPORT void cosmo_rpc_initialization(int toPlugin, int toHost);
+
 #endif // __COSMOPOLITAN__
 
 class RPCPeer {
