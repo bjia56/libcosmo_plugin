85a320d58a2d594c92cbe10ab5a38e2d68236bdb
Brett Jia <dev.bjia56@gmail.com>
2025-01-09 12:12:13 -0500

    add stress test

---
+++ tests/stress/main.cpp
@@ -0,0 +1,82 @@
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <map>
+#include <cassert>
+
+#include "cosmo_plugin.hpp"
+
+void registerTestHandlers(RPCPeer& peer, bool* done) {
+    peer.registerHandler("add", std::function([](int a, int b) -> int {
+        return a + b;
+    }));
+    peer.registerHandler("done", std::function([done]() -> int {
+        *done = true;
+        return 1;
+    }));
+}
+
+void testPeerHandlers(RPCPeer& peer) {
+    for (volatile int i = 0; i < 1000; i++) {
+        int result = peer.call<int>("add", 2, 3);
+        assert(result == 5);
+    }
+    peer.call<int>("done");
+}
+
+#ifdef __COSMOPOLITAN__
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <shared library path>" << std::endl;
+        return 1;
+    }
+
+    std::string cwd = std::filesystem::current_path();
+    std::string objectPath = cwd + "/" + argv[1];
+
+    std::cout << "Populating host functions..." << std::endl;
+    PluginHost plugin(objectPath);
+    bool done = false;
+    registerTestHandlers(plugin, &done);
+
+    std::cout << "Initializing shared library..." << std::endl;
+    plugin.initialize();
+
+    std::cout << "Testing plugin functions..." << std::endl;
+    testPeerHandlers(plugin);
+
+    while(!done) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    std::cout << "Host exiting..." << std::endl;
+    return 0;
+}
+
+#else
+
+void plugin_initializer(Plugin *plugin) {
+    bool *done = new bool;
+    *done = false;
+    registerTestHandlers(*plugin, done);
+
+    std::cout << "Plugin initialized." << std::endl;
+
+    std::thread([plugin, done]() {
+        std::cout << "Testing host functions..." << std::endl;
+        testPeerHandlers(*plugin);
+
+        while(!*done) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+        delete done;
+        std::cout << "Plugin exiting..." << std::endl;
+    }).detach();
+}
+
+#endif
+
