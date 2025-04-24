2e9b8408ab8ca04c51185f42ca510ca2890cbb47
Brett Jia <dev.bjia56@gmail.com>
2025-04-24 06:28:46 -0400

    used shared_ptr to manage pipe lifetime

--- src/cosmo_plugin.cpp
+++ src/cosmo_plugin.cpp
@@ -5,6 +5,7 @@
 #include <cerrno>
 #include <cstring>
 #include <iostream>
+#include <memory>
 #include <random>
 #include <signal.h>
 #include <spawn.h>
@@ -171,11 +172,11 @@ struct PluginHost::impl {
 
     int childPID = 0;
 
-    PipeManager* pipeMgr = nullptr;
+    std::shared_ptr<PipeManager> pipeMgr = nullptr;
 
     ~impl() {
         if (pipeMgr) {
-            delete pipeMgr;
+            pipeMgr->closePipes();
         }
 
         if (dynlibHandle) {
@@ -207,7 +208,9 @@ PluginHost::~PluginHost() {}
 
 void PluginHost::initialize() {
     // Create our pipe manager
-    pimpl->pipeMgr = new PipeManager();
+    // Use a pointer to a shared_ptr to ensure it is not deleted until the thread is done
+    std::shared_ptr<PipeManager> *pipeMgr = new std::shared_ptr<PipeManager>(new PipeManager());
+    pimpl->pipeMgr = *pipeMgr;
 
     if (launchMethod == DLOPEN) {
         // Load the shared object
@@ -247,12 +250,12 @@ void PluginHost::initialize() {
     // Create the transport
     if (IsWindows()) {
         transport.write = [](const void* buffer, size_t size, void* context) -> ssize_t {
-            PipeManager* mgr = static_cast<PipeManager*>(context);
+            auto mgr = *static_cast<std::shared_ptr<PipeManager>*>(context);
             uint32_t bytesWritten;
             return WriteFile(mgr->getHostWriteFD(), buffer, size, &bytesWritten, nullptr) ? bytesWritten : -1;
         };
         transport.read = [](void* buffer, size_t size, void* context) -> ssize_t {
-            PipeManager* mgr = static_cast<PipeManager*>(context);
+            auto mgr = *static_cast<std::shared_ptr<PipeManager>*>(context);
 
             // loop peek until we get data
             while (true) {
@@ -274,27 +277,31 @@ void PluginHost::initialize() {
         };
     } else {
         transport.write = [](const void* buffer, size_t size, void* context) -> ssize_t {
-            PipeManager* mgr = static_cast<PipeManager*>(context);
+            auto mgr = *static_cast<std::shared_ptr<PipeManager>*>(context);
             return write((int)mgr->getHostWriteFD(), buffer, size);
         };
         transport.read = [](void* buffer, size_t size, void* context) -> ssize_t {
-            PipeManager* mgr = static_cast<PipeManager*>(context);
+            auto mgr = *static_cast<std::shared_ptr<PipeManager>*>(context);
             return read((int)mgr->getHostReadFD(), buffer, size);
         };
     }
     transport.close = [](void* context) {
-        PipeManager* mgr = static_cast<PipeManager*>(context);
+        auto mgr = *static_cast<std::shared_ptr<PipeManager>*>(context);
         mgr->closePipes();
     };
-    transport.context = pimpl->pipeMgr;
+    transport.context = pipeMgr;
 
     // Start thread
-   std::thread([this]() {
+   std::thread([this, pipeMgr]() {
         try {
             processMessages();
             //std::cout << "Host thread ended." << std::endl;
         } catch (const std::exception& ex) {
             std::cerr << "Error processing messages: " << ex.what() << std::endl;
+        }
+        // Clean up the pipe manager
+        if (pipeMgr) {
+            delete pipeMgr;
         }
     }).detach();
 }
